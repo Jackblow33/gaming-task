52a934cd5ec706115aae25d8b52c16ea87345928
Jackblow33 <jackblow33@hotmail.com>
2025-05-22 19:02:34 -0400

    Update gnome-0.4.3.sh

--- modules/gnome-0.4.3.sh
+++ modules/gnome-0.4.3.sh
@@ -21,7 +21,7 @@ update_upgrade() {
 # Minimal Gnome packages installation & settings
 install_desktop_environment() {
     #input_file="$SH_PATH/lists/tasksel_pkgs.list"
-    sudo apt-get install -y $(cat "/home/jack/gaming-task/lists/tasksel_pkgs.list") || handle_error
+    sudo apt-get install -y $(cat "/home/$USR/gaming-task/lists/tasksel_pkgs.list") || handle_error
 }
 
 # wireplumber dir and permissions settings
